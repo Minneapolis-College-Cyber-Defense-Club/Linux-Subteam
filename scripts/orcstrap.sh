da473898de51f60116bcf7f0fb85b7cdbc793b95
pernoxiod <pernoxiod@users.noreply.github.com>
2023-02-25 20:04:43 -0600

    big update

--- scripts/orcstrap.sh
+++ scripts/orcstrap.sh
@@ -10,6 +10,7 @@ OS_VER="$(grep '^VERISON_ID=' /etc/os-release | awk -F\" '{print $2}')"
 URL_BASE="https://raw.githubusercontent.com/Minneapolis-College-Cyber-Defense-Club/Linux-Subteam/main"
 SCR_BASE="${DEPOT}/scripts"
 PB_BASE="${DEPOT}/ansible/playbooks"
+COLLECTIONS="ansible.posix community.mysql community.general"
 
 # initial checks
 if [[ $(/bin/whoami) != 'root' ]]; then   
@@ -47,6 +48,14 @@ case $? in
         ;;
 esac
 cp -p /etc/hosts ${QUARANTINE}/
+mv /etc/hosts /etc/hosts.prev
+printf "127.0.0.1 localhost localhost.localdomain\n::1 localhost localhost.localdomain\n">/etc/hosts
+# in case there is more than 1 interface
+for i in $(ifconfig | cut -f1 -d: | grep '^[a-z]' | grep -v lo)
+do
+    printf "$(ifconfig ${i} |grep inet | grep -v inet6 | awk '{print $2}') $(uname -n | cut -f1 -d.) $(uname -n)">>/etc/hosts
+done
+
 cp -p /root/.bash_history ${QUARANTINE}/root.bash_history
 cp -p /etc/ssh/sshd_config ${QUARANTINE}/
 cp -pr /etc/suoders* ${QUARANTINE}/
@@ -59,10 +68,9 @@ do
     [[ -f /var/spool/${c} ]] && rm -f /var/spool/${c}
 done
 
-
 # install required packages
 # need EPEL
-yum install -y epel-release
+yum install -y epel-release libselinux-python
 [[ -x /bin/python ]] || yum install -y python
 [[ -x /bin/ansbile ]] || yum install -y ansible
 [[ -x /bin/wget ]] || yum install -y wget
@@ -71,6 +79,9 @@ PULLER="/bin/wget"
 # pull the things
 ${PULLER} -N -P ${DEPOT}/ansible ${URL_BASE}/ansible
 ${PULLER} -N -P ${DEPOT}/scripts ${URL_BASE}/scripts
+
+# pull the collections
+ansible-galaxy collection install ${COLLECTIONS}
 
 # create things needed
 for u in hal9000 dave2001
@@ -84,36 +95,22 @@ do
     # add more to the vault
     case "${u}" in
         hal9000)
-            printf "uid: 111111\ngid: 111111\n">>${USERVAULT}
+            USERID="111111"
             ;;
         dave2001)
-            printf "uid: 111112\ngid: 111112\n">>${USERVAULT}
+            USERID="111112"
             ;;
     esac
-    ssh-keygen -q -N "" -f ${KEYFILE} -t rsa -b 4096
-
-
-# group creation
-#    grep "${u}" /etc/group
-#    groupadd -g "${gid}" "${u}"
-#    case $? in
-#        0) 
-#            printf "groupadd successful...\n"
-#            grep "${u}" /etc/group
-#            ;;
-#        *)
-#            printf "groupadd failed, fix issue and re-run.\n"
-#            exit 1
-#            ;;
-#    esac
-# user creation
-#    useradd -m -u "${uid}" -g "${gid}" -s/bin/bash -c"${u}" ${u}
-#    case $? in
-#        0) printf "${u} added, generating ssh keys...\n"
-#            ssh-keygen -q -N "" -f ${DEPOT}/keys/${u} -t rsa -b 4096 -m pem
-#            chown ${u}: ${DEPOT}/keys/${u}*
-#            chmod 
+    printf "team_admin: ${u}\nteam_admin_id: ${USERID}\n" >> ${USERVAULT}
+    #ssh-keygen -q -N "" -f ${KEYFILE} -t rsa -b 4096
+    
 
 done
+
+# clean up and hand over
+rm -fr ~/.ansible
+
+# pull the collections
+su -c "ansible-galaxy collection install ${COLLECTIONS}" hal9000
 
 # open the pod bay doors
