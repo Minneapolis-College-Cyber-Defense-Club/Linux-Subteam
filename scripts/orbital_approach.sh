f1676b2d15abc9d3a340ddb49c84e190925673a2
pernoxiod <pernoxiod@users.noreply.github.com>
2023-03-05 15:49:13 -0600

    lots o stuff

--- scripts/orbital_approach.sh
+++ scripts/orbital_approach.sh
@@ -2,7 +2,7 @@
 
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 # global vars
-#DNS="1.1.1.1"
+DNS="1.1.1.1"
 HOSTNAME="$(uname -n)"
 IP="127.0.0.1"
 DEPOT="/depot"
@@ -20,6 +20,47 @@ if [[ $(/bin/whoami) != 'root' ]]; then
     printf "Must run as root.\n"
     exit 666
 fi
+
+# basic networking fixes
+cp /etc/resolv.conf /etc/resolv.conf.orig
+# critical we don't have a poisoned DNS 
+printf "checking dns...\n"
+grep "${DNS}" /etc/resolv.conf 
+case $? in
+    0) 
+        printf "Looks correct.\n" 
+        ;;
+    *) 
+        printf "nameserver ${DNS} \n">/etc/resolv.conf
+        ;;
+esac
+printf "checking /etc/hosts...\n"
+mv /etc/hosts /etc/hosts.orig
+printf "127.0.0.1 localhost localhost.localdomain\n::1 localhost localhost.localdomain\n">/etc/hosts
+# in case there is more than 1 interface
+for i in $(ifconfig | cut -f1 -d: | grep '^[a-z]' | grep -v lo)
+do
+    printf "$(ifconfig ${i} |grep inet | grep -v inet6 | awk '{print $2}') $(uname -n | cut -f1 -d.) $(uname -n)\n">>/etc/hosts
+done
+
+# install pre-reqs
+printf "installing requirements...\n"
+# install required packages
+# need EPEL
+yum install -y epel-release libselinux-python
+[[ -x /bin/python ]] || yum install -y python
+[[ -x /bin/ansbile ]] || yum install -y ansible
+[[ -x /bin/tmux ]] || yum install -y tmux
+[[ -x /bin/wget ]] || yum install -y wget
+[[ -x /bin/git ]] || yum install -y git
+
+###
+## hand-off 
+###
+
+# change passwords / add users
+# create depot
+# 
 
 # create the depot
 printf "generating depot structure..."
@@ -69,7 +110,7 @@ done
 
 # lock less tha uid 1024 accounts that aren't root
 printf "scanning accounts less than uid 1024...\n"
-for u in $(awk -F: '($3 < 1000) {print $1}' /etc/passwd)
+for u in $(awk -F: '($3 < 1024) {print $1}' /etc/passwd)
 do
     if [[ "${u}" != "root" ]]; then
         usermod -L ${u}
@@ -81,31 +122,7 @@ done
 
 
 
-# basic security and functionality checks
-# at this point /etc/resolv.conf should have been fixed via kill chain BUT VALIDATE
-# backup and validate files
-cp /etc/resolv.conf ${QUARANTINE}/
-# critical we don't have a poisoned DNS 
-printf "checking dns...\n"
-grep "${DNS}" /etc/resolv.conf 
-case $? in
-    0) 
-        printf "Looks correct.\n" 
-        ;;
-    *) 
-        mv /etc/resolv.conf ${QUARANTINE}/
-        printf "nameserver ${DNS} \n">/etc/resolv.conf
-        ;;
-esac
-printf "checking /etc/hosts...\n"
-cp -p /etc/hosts ${QUARANTINE}/
-mv /etc/hosts /etc/hosts.prev
-printf "127.0.0.1 localhost localhost.localdomain\n::1 localhost localhost.localdomain\n">/etc/hosts
-# in case there is more than 1 interface
-for i in $(ifconfig | cut -f1 -d: | grep '^[a-z]' | grep -v lo)
-do
-    printf "$(ifconfig ${i} |grep inet | grep -v inet6 | awk '{print $2}') $(uname -n | cut -f1 -d.) $(uname -n)\n">>/etc/hosts
-done
+
 printf "saving copies of other files...\n"
 cp -p /root/.bash_history ${QUARANTINE}/root.bash_history
 cp -p /etc/ssh/sshd_config ${QUARANTINE}/
@@ -121,31 +138,21 @@ done
 printf "quarantine content: \n"
 ls -la ${QUARANTINE}
 
-printf "installing requirements...\n"
-# install required packages
-# need EPEL
-yum install -y epel-release libselinux-python
-[[ -x /bin/python ]] || yum install -y python
-[[ -x /bin/ansbile ]] || yum install -y ansible
-[[ -x /bin/tmux ]] || yum install -y tmux
-[[ -x /bin/wget ]] || yum install -y wget
-[[ -x /bin/git ]] || yum install -y git
+
 
 printf "populating the structure...\n"
 # pull the things
+
+git clone ${GITHOLE}
 for t in ansible scripts
 do
-    git clone ${GITHOLE}
     cp -r ~/Linux-Subteam/${t} ${DEPOT}/
 done
-# make it read-only
-#chmod 0600 ~/Linux-Subteam
 
 # pull the collections
 ansible-galaxy collection install ${COLLECTIONS}
 
+ansible-playbook -i ../inventory/netlab.yml ${PB_BASE}/parking_orbit.yml
 
-ansible-playbook -i 127.0.0.1, ${PB_BASE}/parking_orbit.yml
-
-# pull the collections
+# pull the collections for hal
 su -c "ansible-galaxy collection install ${COLLECTIONS}" hal9000
